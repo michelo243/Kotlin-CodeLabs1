c9015c6d9310df1a10526fd973ca4e1d9d778553
michelo243 <misamuna@gmail.com>
2018-11-09 09:58:20 +0200

    Update Helperdb.kt and Activity Main

--- app/src/main/java/net/synapsehub/kotlin_codelabs1/MainActivity.kt
+++ app/src/main/java/net/synapsehub/kotlin_codelabs1/MainActivity.kt
@@ -2,6 +2,10 @@ package net.synapsehub.kotlin_codelabs1
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
+import net.synapsehub.kotlin_codelabs1.helper.HelperDb
+import net.synapsehub.kotlin_codelabs1.model.Humain
 
 
 class MainActivity : AppCompatActivity() {
@@ -9,6 +13,22 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val context=this
+
+        btn_signup.setOnClickListener({
+            //faire un test si les donnees oont ete saisies
+            if(input_name.text.toString().length>0 && input_email.text.toString().length>0 && input_password.text.toString().length>0){
+                //on cree un variable de type Humain
+                var Toto=Humain(input_name.text.toString(),input_email.text.toString(),input_password.text.toString());
+                //on cree une variable du type Helper pour sqlite
+                var db=HelperDb(context)
+                //on fait appel a la methode d'insertion
+                db.insererDonnees(Toto)
+            }else{
+                Toast.makeText(context,"donnees vides", Toast.LENGTH_LONG).show()
+            }
+        })
 
     }
 }
--- app/src/main/java/net/synapsehub/kotlin_codelabs1/helper/HelperDb.kt
+++ app/src/main/java/net/synapsehub/kotlin_codelabs1/helper/HelperDb.kt
@@ -1,7 +1,53 @@
 package net.synapsehub.kotlin_codelabs1.helper
 
+import android.content.ContentValues
+import android.content.Context
+import android.database.sqlite.SQLiteDatabase
+import android.database.sqlite.SQLiteOpenHelper
+import android.widget.Toast
+import net.synapsehub.kotlin_codelabs1.model.Humain
+
 val DATABASE_NAME="codelabs.db"
 val TABLE_NAME="Users"
 val COL_NAME="name"
 val COL_USERNAME="username"
-val COL_PAASSWORD="password"
+val COL_PASSWORD="password"
+val COL_ID="_id"
+
+
+class HelperDb(var context: Context) :SQLiteOpenHelper(context, DATABASE_NAME, null,1){
+
+
+    override fun onCreate(db: SQLiteDatabase?) {
+        val tableCREATE="CREATE TABLE "+ TABLE_NAME + " (" +
+                COL_ID + " INTEGER PRIMARY KEY AUTOINCREMENT," +
+                COL_NAME + " VARCHAR (100)," +
+                COL_USERNAME + " VARCHAR," +
+                COL_PASSWORD + " VARCHAR)"
+        db?.execSQL(tableCREATE)
+    }
+
+    override fun onUpgrade(db: SQLiteDatabase?, oldVersion: Int, newVersion: Int) {
+
+    }
+
+    fun insererDonnees(humain:Humain){
+        val db=this.writableDatabase
+        val contentValues=ContentValues()
+
+        contentValues.put(COL_NAME,humain.name)
+        contentValues.put(COL_USERNAME,humain.username)
+        contentValues.put(COL_PASSWORD,humain.password)
+
+        var result= db.insert(TABLE_NAME,null,contentValues)
+
+        if(result == -1.toLong())
+            Toast.makeText(context,"Echec d'enregistrement",Toast.LENGTH_LONG).show()
+        else
+            Toast.makeText(context,"Enregistrement reussi",Toast.LENGTH_LONG).show()
+
+
+    }
+
+
+}
